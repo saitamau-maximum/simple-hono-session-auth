8d5577ec28480fed51377ff84dcfd6790f131bb4
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-11-04 22:14:06 +0900

    feat: とりあえずフロントエンドだけを用意

---
+++ index.js
@@ -0,0 +1,38 @@
+const { serve } = require("@hono/node-server");
+const { Hono } = require("hono");
+const templates = require("./templates");
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  const response = templates.HTML("Top Page", templates.TOP_PAGE_TEMPLATE);
+  return c.html(response);
+});
+
+app.get("/login", (c) => {
+  const response = templates.HTML("Login", templates.LOGIN_PAGE_TEMPLATE);
+  return c.html(response);
+});
+
+app.get("/signup", (c) => {
+  const response = templates.HTML("Signup", templates.SIGNUP_PAGE_TEMPLATE);
+  return c.html(response);
+});
+
+app.get("/my-profile", (c) => {
+  // ログインしないとアクセスできないページにする
+  const response = templates.HTML(
+    "User Page",
+    templates.PROFILE_PAGE_TEMPLATE("TODO ここにemailを入れる")
+  );
+  return c.html(response);
+});
+
+serve(app);
+
+process.stdin.on("data", (data) => {
+  if (data.toString().trim() === "q") {
+    db.close();
+    process.exit();
+  }
+});
---
+++ templates.js
@@ -0,0 +1,50 @@
+const { html } = require("hono/html");
+
+const HTML = (title, body) => html`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>${title}</title>
+    </head>
+    <body>
+      ${body}
+    </body>
+  </html>
+`;
+
+const TOP_PAGE_TEMPLATE = html`
+  <h1>認証実装サンプルサイト</h1>
+  <a href="/signup">登録</a>
+  <a href="/login">ログイン</a>
+`;
+
+const LOGIN_PAGE_TEMPLATE = html`
+  <h1>ログイン</h1>
+  <form action="/login" method="post">
+    <input type="text" name="email" placeholder="メールアドレス" />
+    <input type="password" name="password" placeholder="パスワード" />
+    <button type="submit">ログイン</button>
+  </form>
+`;
+
+const SIGNUP_PAGE_TEMPLATE = html`
+  <h1>登録</h1>
+  <form action="/signup" method="post">
+    <input type="text" name="email" placeholder="メールアドレス" />
+    <input type="password" name="password" placeholder="パスワード" />
+    <button type="submit">登録</button>
+  </form>
+`;
+
+const PROFILE_PAGE_TEMPLATE = (email) => html`
+  <h1>ようこそ、${email}さん</h1>
+  <a href="/logout">ログアウト</a>
+`;
+
+module.exports = {
+  HTML,
+  TOP_PAGE_TEMPLATE,
+  LOGIN_PAGE_TEMPLATE,
+  SIGNUP_PAGE_TEMPLATE,
+  PROFILE_PAGE_TEMPLATE,
+};
